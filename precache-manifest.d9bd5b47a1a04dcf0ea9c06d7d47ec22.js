638f56a0a165b14bbf82907d17329f19ed7eb4ab
waldemarciupa <waldemarciupa90@gmail.com>
2020-05-26 08:11:09 +0200

    Updates

---
+++ precache-manifest.d9bd5b47a1a04dcf0ea9c06d7d47ec22.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67dac1a534d4d76bb6c677e2ad72459",
+    "url": "/Oddaj-w-dobre-rece/index.html"
+  },
+  {
+    "revision": "997e6e1cd2b02a7760f3",
+    "url": "/Oddaj-w-dobre-rece/static/js/2.807b3125.chunk.js"
+  },
+  {
+    "revision": "9167fd69ef405e24698737ab39ce0380",
+    "url": "/Oddaj-w-dobre-rece/static/js/2.807b3125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9781a3db453868bd1571",
+    "url": "/Oddaj-w-dobre-rece/static/js/main.80c44a9e.chunk.js"
+  },
+  {
+    "revision": "00285ec247e7bbf9d975",
+    "url": "/Oddaj-w-dobre-rece/static/js/runtime-main.56c2f236.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/Oddaj-w-dobre-rece/static/media/3ColumnsBackground.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddaj-w-dobre-rece/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddaj-w-dobre-rece/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Oddaj-w-dobre-rece/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddaj-w-dobre-rece/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddaj-w-dobre-rece/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddaj-w-dobre-rece/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddaj-w-dobre-rece/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddaj-w-dobre-rece/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Oddaj-w-dobre-rece/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddaj-w-dobre-rece/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddaj-w-dobre-rece/static/media/Signature.e54cfcfc.svg"
+  }
+]);
