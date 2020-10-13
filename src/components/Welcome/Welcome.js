36d1b5e86586a1568055dffb4dbeb3f327cf73c3
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-13 08:18:55 +0200

    adapted welcome section to smaller dispaly screens

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -12,29 +12,24 @@ const StyledWelcomeWrapper = styled.div`
     display: flex;
 `;
 
-// const StyledWelcomeWrapperBackground = styled.div`
-//     display: none;
+const StyledWelcomeWrapperBackground = styled.div`
+    display: none;
 
-//     @media (max-width: 992px) {
-//         display: block;
-//         width: 100%;
-//         height: 70vh;
-//         background-image: url(${props => props.inputBackground || "white"});
-//         background-size: contain;
-//         background-repeat: no-repeat;
-
-//         background-position: center;
-//         z-index: -1;
-//         position: absolute;
-//         top: 50%;
-//         left: 35%;
-//         transform: translate(-50%,-50%);
-//     }
-
-//     @media (max-width: 776px) {
-//         background: none;
-//     }
-// `
+    @media (max-width: 776px) {
+        display: block;
+        width: 100%;
+        height: 50%;
+        background-image: url(${props => props.inputBackground || "white"});
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        z-index: -1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        /* transform: translate(-50%,-50%); */
+    }
+`
 
 const StyledWelcomeLeft = styled.div`
     width: 100%;
@@ -44,9 +39,9 @@ const StyledWelcomeLeft = styled.div`
     background-repeat: no-repeat;
     background-position: center;
 
-    /* @media (max-width: 992px) {
-        background: none;
-    } */
+    @media (max-width: 776px) {
+        display: none;
+    }
 `;
 
 const StyledWelcomeRight = styled.div`
@@ -54,6 +49,14 @@ const StyledWelcomeRight = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+
+
+    /* @media (max-width: 992px) {
+        background-image: url(${props => props.inputBackground || "white"});
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    } */
 `;
 
 const StyledRightWrapper = styled.div`
@@ -93,10 +96,10 @@ const StyledButtonsWrapper = styled.div`
 const Welcome = (currentUser) => {
     return (
         <StyledWelcomeWrapper  >
-            {/* <StyledWelcomeWrapperBackground inputBackground={homeHeroImage} /> */}
             <StyledWelcomeLeft inputBackground={homeHeroImage} />
-            <StyledWelcomeRight>
+            <StyledWelcomeRight inputBackground={homeHeroImage}>
                 <StyledRightWrapper>
+                    <StyledWelcomeWrapperBackground inputBackground={homeHeroImage} />
                     <StyledWelcomeTitle>
                         Zacznij pomagać!<br /> Oddaj niechciane rzeczy w zaufane ręce
                     </StyledWelcomeTitle>
